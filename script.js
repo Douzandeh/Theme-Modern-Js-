93713cfcb90270be1e568b50f56c193db2d4ff58
Hossein <hosseindozandeh799@gmail.com>
2023-01-10 09:58:20 +0330

    Local Storage Color Body

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let themes = document.querySelector('.themes')
 let selection = document.querySelector('#selection')
 
+let selectedTheme = localStorage.getItem("theme") ? localStorage.getItem("theme") : "yellow"
+
+selection.textContent = selectedTheme
+document.body.className = selectedTheme
+
+
 Array.from(themes.children).forEach(theme =>{
     theme.addEventListener('click', e =>{
         let color =e.target.dataset.color
         document.body.className = color
         selection.textContent = color
+        localStorage.setItem("theme" , color)
     })
 })
